f6d1d70e387202e1fd1f6d68cccdd19462818732
sayedSifat <sayedhossainsifat300@gmail.com>
2025-10-02 01:12:25 -0700

    some code are done

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -1,7 +1,43 @@
 import { connectDB } from "@/lib/databaseConnection";
+import { response } from "@/lib/helperFunction";
+import { zSchema } from "@/lib/zodSchema";
+import UserModel from "@/models/User.model";
 
 export async function POST(request) {
   try {
     await connectDB();
+    const validationSchema = zSchema.pick({
+      name: true,
+      email: true,
+      password: true,
+    });
+
+    const payload = await request.json();
+    const validatadData = validationSchema.safeParse(payload);
+
+    if (!validatadData.success) {
+      return response(
+        false,
+        401,
+        "Invalid or missing input fields",
+        validatadData.error
+      );
+    }
+
+    const { name, email, password } = validatadData.data;
+    const checkUser = await UserModel.exists({ email });
+
+    if (checkUser) {
+      return response(true, 409, "User Already Registered");
+    }
+
+    // new Registration
+    const newRegistration = new UserModel({
+      name,
+      email,
+      password,
+    });
+
+    await newRegistration.save();
   } catch (error) {}
 }
