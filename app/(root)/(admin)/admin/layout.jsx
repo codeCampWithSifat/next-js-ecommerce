653483a8faeae2507d8b230fe39e46870ef38727
sayedSifat <sayedhossainsifat300@gmail.com>
2025-10-21 11:06:58 +0600

    some issue are created in media page

--- app/(root)/(admin)/admin/layout.jsx
+++ app/(root)/(admin)/admin/layout.jsx
@@ -6,6 +6,24 @@ import React from "react";
 
 const AdminLayout = ({ children }) => {
   return (
+    // <ThemeProvider
+    //   attribute="class"
+    //   defaultTheme="system"
+    //   enableSystem
+    //   disableTransitionOnChange
+    // >
+    //   <SidebarProvider>
+    //     <AppSidebar />
+    //     <main className="pt-[70px] px-5 min-h-[calc(100vh-40px)] pb-10 bg-background">
+    //       <Topbar />
+    //       {children}
+    //       <div className="border-t h-[40px] flex justify-center items-center bg-gray-50 dark:bg-background text-sm">
+    //         2025 Developer Sifat All Right Reserved
+    //       </div>
+    //     </main>
+    //   </SidebarProvider>
+    // </ThemeProvider>
+
     <ThemeProvider
       attribute="class"
       defaultTheme="system"
@@ -13,28 +31,22 @@ const AdminLayout = ({ children }) => {
       disableTransitionOnChange
     >
       <SidebarProvider>
-        <AppSidebar />
-        <main className="pt-[70px] px-5 min-h-[calc(100vh-40px)] pb-10 bg-background">
-          <Topbar />
-          {children}
-          <div className="border-t h-[40px] flex justify-center items-center bg-gray-50 dark:bg-background text-sm">
-            2025 Developer Sifat All Right Reserved
-          </div>
-        </main>
+        <div className="flex min-h-screen">
+          {/* Sidebar */}
+          <AppSidebar />
+
+          {/* Main Content */}
+          <main className="flex-1 pt-[70px] px-5 pb-10 bg-background">
+            <Topbar />
+            {children}
+            <div className="border-t h-[40px] flex justify-center items-center bg-gray-50 dark:bg-background text-sm">
+              2025 Developer Sifat All Right Reserved
+            </div>
+          </main>
+        </div>
       </SidebarProvider>
     </ThemeProvider>
   );
 };
 
 export default AdminLayout;
-
-//  <SidebarProvider>
-//       <AppSidebar />
-//       <div className="flex flex-col min-h-screen bg-background md:ml-64">
-//         <Topbar />
-//         <main className="flex-1 pt-[70px] px-5 pb-10">{children}</main>
-//         <footer className="border-t h-[40px] flex justify-center items-center bg-gray-50 dark:bg-background text-sm">
-//           © 2025 Developer Sifat — All Rights Reserved
-//         </footer>
-//       </div>
-//     </SidebarProvider>
--- app/(root)/(admin)/admin/media/page.jsx
+++ app/(root)/(admin)/admin/media/page.jsx
@@ -1,5 +1,6 @@
 import BreadCrumb from "@/components/Application/Admin/BreadCrumb";
 import UploadMedia from "@/components/Application/Admin/UploadMedia";
+import { Card, CardHeader } from "@/components/ui/card";
 import { ADMIN_DASHBOARD } from "@/routes/AdminPanelRoutes";
 
 const breadcrumbData = [
@@ -17,7 +18,18 @@ const MediaPage = () => {
   return (
     <div>
       <BreadCrumb breadcrumbData={breadcrumbData} />
-      <UploadMedia />
+
+      <Card className="border border-red-500">
+        <CardHeader className="pt-3 px-3 border border-b [.border-b]:pb-2">
+          <div className="flex justify-between w-full items-center">
+            <h4 className="font-semibold text-xl uppercase">Media</h4>
+
+            <div className="flex items-center gap-5">
+              <UploadMedia />
+            </div>
+          </div>
+        </CardHeader>
+      </Card>
     </div>
   );
 };
