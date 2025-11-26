e102d9704cf92423a2376971883d66803472e77c
sayedSifat <sayedhossainsifat300@gmail.com>
2025-11-26 14:45:59 +0600

    media are done

--- app/(root)/(admin)/admin/media/edit/[id]/page.jsx
+++ app/(root)/(admin)/admin/media/edit/[id]/page.jsx
@@ -1,10 +1,27 @@
 "use client";
 import BreadCrumb from "@/components/Application/Admin/BreadCrumb";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import useFetch from "@/hooks/useFetch";
 import { ADMIN_DASHBOARD, ADMIN_MEDIA_SHOW } from "@/routes/AdminPanelRoutes";
 import { useParams } from "next/navigation";
-import { use } from "react";
+import { use, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { zSchema } from "@/lib/zodSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import ButtonLoading from "@/components/Application/ButtonLoading";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import placeHolder from "../../../../../../../public/assets/images/img-placeholder.webp";
+import { showToast } from "@/lib/showToast";
+import axios from "axios";
 
 const breadCrumbData = [
   {
@@ -23,11 +40,53 @@ const breadCrumbData = [
 
 const EditMedia = ({ params }) => {
   const { id } = use(params);
-  console.log("Edit Page", id);
+  const [loading, setLoading] = useState(false);
 
   const { data: mediaData } = useFetch(`/api/media/get/${id}`);
 
-  console.log("Media Data", mediaData);
+  // console.log("Media Data", mediaData);
+
+  const formSchema = zSchema.pick({
+    _id: true,
+    alt: true,
+    title: true,
+  });
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      _id: "",
+      alt: "",
+      title: "",
+    },
+  });
+
+  useEffect(() => {
+    if (mediaData && mediaData.success) {
+      const data = mediaData.data;
+      form.reset({
+        _id: data._id,
+        alt: data.alt,
+        title: data.title,
+      });
+    }
+  }, [mediaData]);
+
+  const onSubmit = async (values) => {
+    try {
+      setLoading(true);
+      const { data: response } = await axios.put(`/api/media/update`, values);
+      if (!response.success) {
+        throw new Error(response.message);
+      }
+
+      showToast("success", response.message);
+    } catch (error) {
+      showToast("error", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div>
       <BreadCrumb breadcrumbData={breadCrumbData} />
@@ -37,7 +96,76 @@ const EditMedia = ({ params }) => {
           <h4 className="font-semibold text-xl">Edit Media</h4>
         </CardHeader>
 
-        <CardContent className="pb-5"></CardContent>
+        <CardContent className="pb-5 size-full">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div>
+                <Image
+                  src={mediaData?.data?.secure_url || placeHolder}
+                  height={150}
+                  width={150}
+                  alt="I love you"
+                />
+              </div>
+              <div className="mb-3 ">
+                <FormField
+                  control={form.control}
+                  name="alt"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Alt</FormLabel>
+                      <FormControl>
+                        <div>
+                          <Input
+                            type="text"
+                            className="w-full"
+                            placeholder="Enter Alt"
+                            {...field}
+                          />
+                        </div>
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div className="mb-3">
+                <FormField
+                  control={form.control}
+                  name="title"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Title</FormLabel>
+                      <FormControl>
+                        <div>
+                          <Input
+                            type="text"
+                            className="w-full"
+                            placeholder="title"
+                            {...field}
+                          />
+                        </div>
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div className="mb-3">
+                <ButtonLoading
+                  loading={loading}
+                  type="submit"
+                  text="Edit Media"
+                  className=""
+                />
+              </div>
+            </form>
+          </Form>
+        </CardContent>
       </Card>
     </div>
   );
